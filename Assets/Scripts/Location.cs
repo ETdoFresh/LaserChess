51b0ed487e25aef682c671294bb669f20616b577
GitHub Action <etdofresh@gmail.com>
2021-04-12 23:21:12 +0000

    Update gh-pages. Auto commit from build

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Location : MonoBehaviour
+{
+    [SerializeField] private Vector2 size = new Vector2(9, 9);
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+
+    private void Update()
+    {
+        var offset = (-size + Vector2.one) / 2;
+        transform.position = new Vector2(x, y) + offset;
+    }
+}
