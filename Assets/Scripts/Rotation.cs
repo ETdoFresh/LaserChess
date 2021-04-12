67524cad75d06a6733ddd0be7228234117db835a
ETdoFresh <ETdoFresh@gmail.com>
2021-04-12 17:09:11 -0500

    Add Initial Project

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private int rotation;
+
+    private void Update()
+    {
+        transform.eulerAngles = new Vector3(0, 0, rotation * 90);
+    }
+}
